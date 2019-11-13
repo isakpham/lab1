e78d8dd84fa24b5173c6cebf023a7aef59b53a5e
Isak Marklund <iskmar-9@student.ltu.se>
2019-11-13 17:27:40 +0100

    Fixed the program

--- Lab1Task5/Task5.cpp
+++ Lab1Task5/Task5.cpp
@@ -2,28 +2,65 @@
 
 using namespace std;
 
-int AskIfMarried(){
-	char quesstion;
-	printf("Are you married? y/n");
-	scanf("%c",&quesstion);
-	printf("%c", quesstion);
-	if (quesstion == 'y' )
-	{
-		return 1;
-	}
-	return 0;
-}
+
 
 int main() {
-	char name[20] = scanf("%c", name);
-	printf("Whats your name?");
+	char name[20], address[20], gender ,nm, adrs, question;
+	int age, married;
+	float height;
 	
+
+	printf("Whats your name? ");
+	scanf("%s", name);
+	nm = getchar();
 	
-	for (int i = 0; i < sizeof(name); i++)
+	printf("What's your gender? (m/f) ");
+	scanf("%c", &gender);
+	
+	printf("How old are you? ");
+	scanf("%d", &age);
+
+	
+
+	printf("Whats your address? ");
+	scanf("%s", &address);
+	adrs = getchar();
+
+	printf("are you married? (y/n) ");
+	scanf("%c", &question);
+	if (question == 'y')
 	{
-		printf("%c", name[i]);
+		married = 1;
+	}
+	else
+	{
+		married = 0;
+	}
+	printf("How tall are you?(in metres) ");
+	scanf("%f", &height);
+	
+	printf("your name is: " "%s", name);
+	printf("\n");
+	printf("you are: " "%d", age);
+	printf(" years old");
+	printf("\n");
+	printf("you are: " "%f", height);
+	printf(" metres tall");
+	printf("\n");
+
+	if (married == 1)
+	{
+		printf("You're married");
+		printf("\n");
+	}
+	else
+	{
+		printf("you're not married");
+		printf("\n");
 	}
 	
+	printf("you live at: " "%s", address);
+	printf("\n");
 	
 	
 	
